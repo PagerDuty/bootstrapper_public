5452fdaacd3785eeabf611d8db53876b7c589960
Jason Gladish <jason@expectedbehavior.com>
2009-12-20 16:28:57 -0500

    adding less verbose and more colorful status output

--- lib/bootstrapper.rb
+++ lib/bootstrapper.rb
@@ -5,11 +5,25 @@ class Bootstrapper
   def self.for(key, &block)
     tasks[key] = block
   end
-
+  
+  @depth = 0
+  
+  def self.run_start(key)
+    print "\033[1;31;40mBootstrapping\e[0m >>" if @depth == 0
+    print " [\033[1;33;40m:#{key}\e[0m"
+    @depth += 1
+  end
+  
+  def self.run_end(key)
+    @depth -= 1
+    print "]"
+    puts "\r\n\033[1;31;40mBootstrapping\e[0m >> \033[1;32;40mDone\e[0m" if @depth == 0
+  end
+  
   def self.run(key)
-    puts ">> Started executing bootstrap for #{key}"
+    self.run_start(key)
     tasks[key].call(self)
-    puts ">> Finished executing bootstrap for #{key}"
+    self.run_end(key)
   end
 
   def self.truncate_tables(*tables)
