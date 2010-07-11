e98494f6b9000e99b3edb932a4031a88ed45506b
Jan Schwenzien <jan@schwenzien.info>
2010-07-11 14:48:06 +0200

    minor generator fix

--- lib/generators/bootstrapper_generator.rb
+++ lib/generators/bootstrapper_generator.rb
@@ -2,6 +2,6 @@ class BootstrapperGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_files
-    copy_file 'bootstrap.erb', 'db/bootstrap.rb'
+    template 'bootstrap.erb', 'db/bootstrap.rb'
   end
 end
