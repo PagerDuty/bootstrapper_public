c7ea41355d99c0a1bb3fe9e2e5096a0b2117d92c
Jan Schwenzien <jan@schwenzien.info>
2010-08-31 00:11:54 +0200

    fixed conflict with database_cleaner

--- lib/connection_adapters/mysql_adapter.rb
+++ lib/connection_adapters/mysql_adapter.rb
@@ -2,12 +2,16 @@ module ActiveRecord
 
   module ConnectionAdapters
 
-    class MysqlAdapter
-
+    class MysqlAdapter < AbstractAdapter
       def truncate_table(table_name)
-        execute "TRUNCATE TABLE #{table_name}"
+        execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
       end
+    end
 
+    class Mysql2Adapter < AbstractAdapter
+      def truncate_table(table_name)
+        execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
+      end
     end
 
   end
