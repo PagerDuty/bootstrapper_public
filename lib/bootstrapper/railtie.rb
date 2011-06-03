a6f3bf0640cbe7ff6e175586145c94ee73331a75
Ken Pratt <ken@kenpratt.net>
2011-06-02 17:50:45 -0700

    Don't need to subscribe to change events.

--- lib/bootstrapper/railtie.rb
+++ lib/bootstrapper/railtie.rb
@@ -5,11 +5,6 @@ class Bootstrapper
     # configure our plugin on boot. other extension points such
     # as configuration, rake tasks, etc, are also available
     initializer "bootstrapper.initialize" do |app|
-      # subscribe to all rails notifications: controllers, AR, etc.
-      ActiveSupport::Notifications.subscribe do |*args|
-        event = ActiveSupport::Notifications::Event.new(*args)
-        puts "Got notification: #{event.inspect}"
-      end
     end
 
     ActiveSupport.on_load :active_record do
