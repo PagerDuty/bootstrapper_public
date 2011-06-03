336c478915a7a8f307ae22cc35605fe06d67e531
Ken Pratt <ken@kenpratt.net>
2011-06-02 17:04:08 -0700

    Put superclasses back into mysql adapter.

--- lib/connection_adapters/mysql_adapter.rb
+++ lib/connection_adapters/mysql_adapter.rb
@@ -1,12 +1,12 @@
 module ActiveRecord
   module ConnectionAdapters
-    class MysqlAdapter
+    class MysqlAdapter < AbstractAdapter
       def truncate_table(table_name)
         execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
       end
     end
 
-    class Mysql2Adapter
+    class Mysql2Adapter < AbstractAdapter
       def truncate_table(table_name)
         execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
       end
